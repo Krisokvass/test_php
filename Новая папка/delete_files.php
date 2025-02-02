53977765a0355fc00bccd6a7b83adeed1a583e1e
Krisokvass <kristinaaralovp@gmail.com>
2025-02-02 17:03:53 +0300

    Delete Новая папка/delete_files.php

--- Новая папка/delete_files.php
+++
@@ -1,9 +0,0 @@
-php<?php
-$filenames = ['2.txt', '3.txt', '4.txt', '5.txt'];
-foreach ($filenames as $file) {
-    if (file_exists($file)) {
-        unlink($file);
-    }
-}
-echo "Файлы успешно удалены.";
-?>
